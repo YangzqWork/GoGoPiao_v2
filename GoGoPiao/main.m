3700e84dd0f758becbe22448cbda7aeccf4eb177
Cho-Yeung Lam <apple@Cho-Yeung-Lam.local>
2013-07-14 16:59:29 +0800

    init

---
+++ GoGoPiao/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoGoPiao
+//
+//  Created by Cho-Yeung Lam on 14/7/13.
+//  Copyright (c) 2013 Cho-Yeung Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
