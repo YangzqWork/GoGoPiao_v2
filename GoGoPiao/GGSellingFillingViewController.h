6d3abc14cf9d9ec2fb416bac741a523717b243b4
Cho-Yeung Lam <jackie_lam@foxmail.com>
2013-08-15 15:20:19 +0800

    修复

--- GoGoPiao/GGSellingFillingViewController.h
+++ GoGoPiao/GGSellingFillingViewController.h
@@ -2,16 +2,12 @@
 //  GGSellingFillingViewController.h
 //  GoGoPiao
 //
-//  Created by Cho-Yeung Lam on 12/8/13.
+//  Created by Cho-Yeung Lam on 15/8/13.
 //  Copyright (c) 2013 Cho-Yeung Lam. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface GGSellingFillingViewController : UIViewController<RETableViewManagerDelegate>
-
-@property (strong, nonatomic) RETableViewManager *tableViewManager;
-@property (strong, nonatomic) RETableViewSection *firstTableViewSection;
-@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@interface GGSellingFillingViewController : UIViewController
 
 @end
--- GoGoPiao/GGSellingFillingViewController.m
+++ GoGoPiao/GGSellingFillingViewController.m
@@ -2,7 +2,7 @@
 //  GGSellingFillingViewController.m
 //  GoGoPiao
 //
-//  Created by Cho-Yeung Lam on 12/8/13.
+//  Created by Cho-Yeung Lam on 15/8/13.
 //  Copyright (c) 2013 Cho-Yeung Lam. All rights reserved.
 //
 
@@ -26,16 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    self.tableViewManager = [[RETableViewManager alloc] initWithTableView:self.tableView delegate:self];
-    self.firstTableViewSection = [[RETableViewSection alloc] initWithHeaderTitle:@"请输入票面信息"];
-    [self.tableViewManager addSection:self.firstTableViewSection];
-    [self.firstTableViewSection addItem:[RETextItem itemWithTitle:@"座位分区" value:nil placeholder:@"请输入座位分区"]];
-    [self.firstTableViewSection addItem:[RETextItem itemWithTitle:@"排号" value:nil placeholder:@"请输入排号"]];
-    [self.firstTableViewSection addItem:[RETextItem itemWithTitle:@"数量" value:nil placeholder:@"请输入数量"]];
-    [self.firstTableViewSection addItem:[RETextItem itemWithTitle:@"座位号" value:nil placeholder:@"请输入座位"]];
-    [self.firstTableViewSection addItem:[RETextItem itemWithTitle:@"每张价格" value:nil placeholder:@"请输入每张价格"]];
-    [self.firstTableViewSection addItem:[RETextItem itemWithTitle:@"拆分方式" value:nil placeholder:@"请输入拆分方式"]];
+    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,8 +35,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)viewDidUnload {
-    [self setTableView:nil];
-    [super viewDidUnload];
-}
 @end
