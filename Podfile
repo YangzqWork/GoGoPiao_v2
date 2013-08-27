3f0fd2cae37adf82164f83e025aec5ec9d01f1c4
Cho-Yeung Lam <jackie_lam@foxmail.com>
2013-08-27 11:48:55 +0800

    cocoapods

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, '6.0'
+pod 'RETableViewManager', '~> 1.1.1'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// REFormattedNumberField
+#define COCOAPODS_POD_AVAILABLE_REFormattedNumberField
+#define COCOAPODS_VERSION_MAJOR_REFormattedNumberField 1
+#define COCOAPODS_VERSION_MINOR_REFormattedNumberField 0
+#define COCOAPODS_VERSION_PATCH_REFormattedNumberField 4
+
+// RETableViewManager
+#define COCOAPODS_POD_AVAILABLE_RETableViewManager
+#define COCOAPODS_VERSION_MAJOR_RETableViewManager 1
+#define COCOAPODS_VERSION_MINOR_RETableViewManager 1
+#define COCOAPODS_VERSION_PATCH_RETableViewManager 3
+
